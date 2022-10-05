6ecb03606e09f5c39d7c65aaaf2184a4f2e6dc48
dshzhkv <dashunich.f@gmail.com>
2022-10-05 13:12:59 +0500

    create components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,6 +8,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilmName="The Grand Budapest Hotel"
+      promoFilmGenre="Drama"
+      promoFilmYear={2014}
+      promoFilmPosterImgSrc="img/the-grand-budapest-hotel-poster.jpg"
+      promoFilmBackgroundImgSrc="img/bg-the-grand-budapest-hotel.jpg"
+    />
   </React.StrictMode>,
 );
