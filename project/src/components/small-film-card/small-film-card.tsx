6b4d9050aec53c073afff2fc35742178bdc42509
dshzhkv <dashunich.f@gmail.com>
2022-11-09 19:12:10 +0500

    add video player

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -1,11 +1,39 @@
 import {FilmInfo} from '../../types/FilmInfo';
 import {Link} from 'react-router-dom';
+import {RefObject, useEffect, useRef, useState} from "react";
+import VideoPlayer from "../video-player/video-player";
 
 function SmallFilmCard({film}: {film: FilmInfo}):JSX.Element {
+
+  const [isPlayerOn, setIsPlayerOn] = useState(false);
+  const ref = useRef(null);
+
+  const handleMouseOver = () => setTimeout(() => setIsPlayerOn(true), 1000);
+  const handleMouseOut = () => setIsPlayerOn(false);
+
+  useEffect(() => {
+      // @ts-ignore
+      let card : EventTarget = ref.current;
+      if (card) {
+        card.addEventListener("mouseover", handleMouseOver);
+        card.addEventListener("mouseout", handleMouseOut);
+
+        return () => {
+          card.removeEventListener("mouseover", handleMouseOver);
+          card.removeEventListener("mouseout", handleMouseOut);
+        };
+      }
+    }
+  );
+
   return (
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card" ref={ref}>
       <div className="small-film-card__image">
-        <img src={film.posterImgSrc} alt={film.name} width="280" height="175"/>
+        {
+          isPlayerOn ?
+            <VideoPlayer videoSrc={film.videoSrc} posterSrc={film.posterImgSrc}/> :
+            <img src={film.posterImgSrc} alt={film.name} width="280" height="175"/>
+        }
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>{film.name}</Link>
